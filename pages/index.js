24adf12aee2ea2720cf39b88d8a2cb25ed40f9d4
Erick Madduma <emadduma@Arcanys.com>
2022-12-17 19:51:00 +0800

    list page with roles

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
-import Link from 'next/link'
-import useSWR from 'swr'
-import { Auth, Card, Typography, Space, Button, Icon } from '@supabase/ui'
-import { supabase } from '../lib/initSupabase'
-import { useEffect, useState } from 'react'
+import Link from 'next/link';
+import useSWR from 'swr';
+import { Auth, Card, Typography, Space, Button, Icon } from '@supabase/ui';
+import { supabase } from '../lib/initSupabase';
+import { useEffect, useState } from 'react';
 
 const fetcher = (url, token) =>
   fetch(url, {
     method: 'GET',
     headers: new Headers({ 'Content-Type': 'application/json', token }),
     credentials: 'same-origin',
-  }).then((res) => res.json())
+  }).then((res) => res.json());
 
 const Index = () => {
-  const { user, session } = Auth.useUser()
+  const { user, session } = Auth.useUser();
   const { data, error } = useSWR(
     session ? ['/api/getUser', session.access_token] : null,
     fetcher
-  )
-  const [authView, setAuthView] = useState('sign_in')
+  );
+  const [authView, setAuthView] = useState('sign_in');
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
       (event, session) => {
-        if (event === 'PASSWORD_RECOVERY') setAuthView('update_password')
+        if (event === 'PASSWORD_RECOVERY') setAuthView('update_password');
         if (event === 'USER_UPDATED')
-          setTimeout(() => setAuthView('sign_in'), 1000)
+          setTimeout(() => setAuthView('sign_in'), 1000);
         // Send session to /api/auth route to set the auth cookie.
         // NOTE: this is only needed if you're doing SSR (getServerSideProps)!
         fetch('/api/auth', {
@@ -32,14 +32,14 @@ const Index = () => {
           headers: new Headers({ 'Content-Type': 'application/json' }),
           credentials: 'same-origin',
           body: JSON.stringify({ event, session }),
-        }).then((res) => res.json())
+        }).then((res) => res.json());
       }
-    )
+    );
 
     return () => {
-      authListener.unsubscribe()
-    }
-  }, [])
+      authListener.unsubscribe();
+    };
+  }, []);
 
   const View = () => {
     if (!user)
@@ -63,7 +63,7 @@ const Index = () => {
             socialButtonSize="xlarge"
           />
         </Space>
-      )
+      );
 
     return (
       <Space direction="vertical" size={6}>
@@ -101,12 +101,14 @@ const Index = () => {
 
             <Typography.Text>
               <Link href="/profile">SSR example with getServerSideProps</Link>
+              <br />
+              <Link href="/list">This is an authenticated route</Link>
             </Typography.Text>
           </>
         )}
       </Space>
-    )
-  }
+    );
+  };
 
   return (
     <div style={{ maxWidth: '420px', margin: '96px auto' }}>
@@ -114,7 +116,7 @@ const Index = () => {
         <View />
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
